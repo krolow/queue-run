d3bac305364d49772f677cfac29abc3f21e1b041
Assaf Arkin <assaf@labnotes.org>
2021-12-06 09:58:31 -0800

    Fixed: Lambda runtime should be nodejs14.x, type was wrong here

--- packages/builder/src/util/getRuntime.ts
+++ packages/builder/src/util/getRuntime.ts
@@ -1,4 +1,3 @@
-import { Runtime } from "@aws-sdk/client-lambda";
 import { JscTarget } from "@swc/core";
 import { readFile } from "fs/promises";
 import path from "path";
@@ -12,7 +11,8 @@ export default async function getRuntime(dirname: string): Promise<{
   // Primary Node version (12, 14, 16, etc)
   nodeVersion: "12" | "14";
   // Lambda runtime version, eg nodejs14.x
-  lambdaRuntime: keyof typeof Runtime;
+  // 2021-12-06: keyof typeof Runtime has "nodejs14x", but Lambda expects "nodejs14.x"  ¯\_(ツ)_/¯
+  lambdaRuntime: string;
   // Tell SWC which Node version to compile for
   jscTarget: JscTarget;
 }> {
@@ -20,15 +20,11 @@ export default async function getRuntime(dirname: string): Promise<{
     await readFile(path.join(dirname, "package.json"), "utf8")
   );
   const specifiedEngine = packageJSON.engines?.node;
-  if (!specifiedEngine)
-    return {
-      ...defaultRuntime,
-      lambdaRuntime: `nodejs${defaultRuntime.nodeVersion}x`,
-    };
+  if (!specifiedEngine) return defaultRuntime;
 
   for (const runtime of runtimes) {
     if (semver.satisfies(`${runtime.nodeVersion}.0.0`, specifiedEngine))
-      return { ...runtime, lambdaRuntime: `nodejs${runtime.nodeVersion}x` };
+      return runtime;
   }
   throw new Error(
     `package.json specifies unsupported Node engine ${specifiedEngine}`
@@ -37,10 +33,11 @@ export default async function getRuntime(dirname: string): Promise<{
 
 const runtimes: Array<{
   nodeVersion: "12" | "14";
+  lambdaRuntime: string;
   jscTarget: JscTarget;
 }> = [
-  { nodeVersion: "14", jscTarget: "es2020" },
-  { nodeVersion: "12", jscTarget: "es3" },
+  { nodeVersion: "14", jscTarget: "es2020", lambdaRuntime: "nodejs14.x" },
+  { nodeVersion: "12", jscTarget: "es3", lambdaRuntime: "nodejs12.x" },
 ];
 
 const defaultRuntime = runtimes[0];
