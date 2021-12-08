979c86e82cba020063f0f3522f772a0a6f1ac346
Assaf Arkin <assaf@labnotes.org>
2021-12-08 10:16:44 -0800

    Comments on bearer token algorithm

--- packages/server/app/createBearerToken.ts
+++ packages/server/app/createBearerToken.ts
@@ -1,11 +1,21 @@
 import crypto from "crypto";
 
+// Bearer token: client app sends this to the server in HTTP Authorization
+// header, 40 characters long
+//
+// Token ID: SHA256 of the bearer token, 32 characters long
+
+const tokenIDLength = 32;
+
 export default function createBearerToken() {
-  const bearerToken = crypto.pseudoRandomBytes(32).toString("base64");
+  const bearerToken = crypto
+    .pseudoRandomBytes(32)
+    .toString("base64")
+    .slice(0, 40);
   const tokenId = crypto
     .createHash("sha256")
     .update(bearerToken)
     .digest("hex")
-    .slice(0, 32);
+    .slice(0, tokenIDLength);
   return { bearerToken, tokenId };
 }
