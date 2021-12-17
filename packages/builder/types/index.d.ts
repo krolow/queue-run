688935162075797fb0b9366489f54c5d26b7975b
Assaf Arkin <assaf@labnotes.org>
2021-12-17 13:07:56 -0800

    Upgraded dependencies

--- packages/builder/types/index.d.ts
+++ packages/builder/types/index.d.ts
@@ -1,1 +1,28 @@
-export * from "../src";
+/// <reference types="node" />
+import Lambda from '@aws-sdk/client-lambda';
+import swc from '@swc/core';
+
+declare function buildProject({ full, signal, sourceDir, targetDir, }: {
+    full?: boolean;
+    signal?: AbortSignal;
+    sourceDir: string;
+    targetDir: string;
+}): Promise<{
+    lambdaRuntime: Lambda.Runtime;
+    queues: string[];
+    zip?: Uint8Array;
+}>;
+
+declare type RuntimeVersion = {
+    nodeVersion: "12" | "14";
+    lambdaRuntime: Lambda.Runtime;
+    jscTarget: swc.JscTarget;
+};
+declare function getRuntime(dirname: string): Promise<RuntimeVersion>;
+
+declare function moduleLoader({ dirname: dirname, watch, }: {
+    dirname: string;
+    watch: boolean;
+}): Promise<void>;
+
+export { buildProject, getRuntime, moduleLoader };
