a30b18759481f1d796a67656988c847da3797ab0
Assaf Arkin <assaf@labnotes.org>
2022-01-01 11:01:59 -0800

    Build in sequence

--- packages/example/lib/capture.ts
+++ packages/example/lib/capture.ts
@@ -1,5 +1,6 @@
 export default async function capture(url: string): Promise<string> {
   // This could take several seconds, sometimes it would hang on for a minute or
   // two, sometimes fail
+  console.info('📸 Capturing "%s"', url);
   return "screenshot.png";
 }
