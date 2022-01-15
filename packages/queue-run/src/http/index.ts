c032026a558dc355e5bb3cb6252b15c041e895c5
Assaf Arkin <assaf@labnotes.org>
2022-01-15 09:54:49 -0800

    Fixed: broken export from queue-run

--- packages/queue-run/src/http/index.ts
+++ packages/queue-run/src/http/index.ts
@@ -1,6 +1,5 @@
 export * from "./exports.js";
-export { fetch, Headers, Request, Response } from "./fetch.js";
-export { Blob, default as form, File } from "./form.js";
+export { Blob, fetch, File, Headers, Request, Response } from "./fetch.js";
 export { default as handleHTTPRequest } from "./handler.js";
 export * from "./middleware.js";
 export { default as url } from "./url.js";
