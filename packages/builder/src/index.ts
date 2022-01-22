ca1622c8c9dfbfdd11bf0f22df65ece3e8c202fb
Assaf Arkin <assaf@labnotes.org>
2022-01-22 12:46:57 -0800

    Fixed: this hack to load JSON doc

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -1,8 +1,6 @@
+import { createRequire } from "module";
 export * from "./build/index.js";
 export * from "./deploy/index.js";
 export * from "./setup/index.js";
 
-import fs from "node:fs/promises";
-export const policy = JSON.parse(
-  await fs.readFile(new URL("./policy.json", import.meta.url).pathname, "utf-8")
-);
+export const policy = createRequire(import.meta.url)("./policy.json");
