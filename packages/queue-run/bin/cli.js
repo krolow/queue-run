3287761e35e67cfb7e0a45a83439ffd5a64b7dab
Assaf Arkin <assaf@labnotes.org>
2022-01-22 13:17:03 -0800

    Another hack using createRequire

--- packages/queue-run/bin/cli.js
+++ packages/queue-run/bin/cli.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 import { execFileSync } from "node:child_process";
+import { createRequire } from "node:module";
 
 // CLI is provided by queue-run-cli, separate package, keep queue-run smaller.
 const cliModule = "queue-run-cli";
 try {
-  await import(cliModule);
+  createRequire(import.meta.url).resolve(cliModule);
 } catch (error) {
-  if (error instanceof Error && error.code === "ERR_MODULE_NOT_FOUND") {
+  if (error instanceof Error && error.code === "MODULE_NOT_FOUND") {
     // On first use, we install queue-run-cli. We don't update package.json, if you
     // nuke node_modules, we'll just install again.
     console.info("First run: installing %s ...\n", cliModule);
@@ -17,6 +18,7 @@ try {
         stdio: "inherit",
       }
     );
-    await import(cliModule);
   } else throw error;
 }
+
+await import(cliModule);
