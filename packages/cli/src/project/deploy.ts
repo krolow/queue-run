9276565e5f28c33e823b4dd7ba2101da85f4e266
Assaf Arkin <assaf@labnotes.org>
2022-01-22 13:46:30 -0800

    Cleanup cli deploy

--- packages/cli/src/project/deploy.ts
+++ packages/cli/src/project/deploy.ts
@@ -14,38 +14,34 @@ const command = new Command("deploy")
   .description("deploy your project")
   .action(async () => {
     const { name } = await loadProject();
-    await deployRuntimeLambda({ name });
+    const accountId = await getAccountId();
+    const region = process.env.AWS_REGION || "us-east-1";
+
+    const spinner = ora("Setting up API Gateway...").start();
+    const { httpUrl, wsUrl, wsApiId } = await setupAPIGateway(name);
+    spinner.succeed("Created API Gateway endpoints");
+
+    const lambdaArn = await deployLambda({
+      buildDir: ".queue-run",
+      sourceDir: process.cwd(),
+      config: {
+        accountId,
+        env: "production",
+        region,
+        slug: name,
+        httpUrl,
+        wsUrl,
+        wsApiId,
+      },
+    });
+    await setupIntegrations({ project: name, lambdaArn });
+
+    console.info(chalk.bold.green(`Your API is available at:\t%s`), httpUrl);
+    console.info(chalk.bold.green(`WebSocket available at:\t\t%s`), wsUrl);
+    console.info(`Try:\n  curl ${httpUrl}`);
   });
 
 export default command;
-
-async function deployRuntimeLambda({ name }: { name: string }) {
-  const accountId = await getAccountId();
-  const region = process.env.AWS_REGION || "us-east-1";
-
-  const spinner = ora("Setting up API Gateway...").start();
-  const { httpUrl, wsUrl, wsApiId } = await setupAPIGateway(name);
-  spinner.succeed("Created API Gateway endpoints");
-
-  const lambdaArn = await deployLambda({
-    buildDir: ".queue-run",
-    sourceDir: process.cwd(),
-    config: {
-      accountId,
-      env: "production",
-      region,
-      slug: name,
-      httpUrl,
-      wsUrl,
-      wsApiId,
-    },
-  });
-  await setupIntegrations({ project: name, lambdaArn });
-
-  console.info(chalk.bold.green(`Your API is available at:\t%s`), httpUrl);
-  console.info(chalk.bold.green(`WebSocket available at:\t\t%s`), wsUrl);
-  console.info(`Try:\n  curl ${httpUrl}`);
-}
 
 async function getAccountId(): Promise<string> {
   const iam = new IAM({});
