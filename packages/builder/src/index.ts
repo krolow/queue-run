3c7dab869c36805154ca8f65f1c288b99849e154
Assaf Arkin <assaf@labnotes.org>
2022-01-22 13:03:22 -0800

    Fixed: builder can't find json import

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -3,4 +3,6 @@ export * from "./build/index.js";
 export * from "./deploy/index.js";
 export * from "./setup/index.js";
 
-export const policy = createRequire(import.meta.url)("./policy.json");
+// This is bullshit, but Node 14 has no way to import JSON as module, and tsc
+// doesn't see the import so won't copy the file to the dist directory.
+export const policy = createRequire(import.meta.url)("../src/policy.json");
