4855745c4a23bae8d597598187dd4628291408ce
Assaf Arkin <assaf@labnotes.org>
2022-01-28 12:39:40 -0800

    Comment

--- packages/queue-run/src/shared/authenticated.ts
+++ packages/queue-run/src/shared/authenticated.ts
@@ -1,5 +1,16 @@
 import { getLocalStorage } from "..";
 
+/**
+ * Call this to autheticate a user.
+ *
+ * The `authenticate` method should return a user object (authenticated) or null
+ * (anonymous). Alternatively, but you don't have to, you can call this method.
+ *
+ * This method is useful for authenticating WebSocket connections during the
+ * `onConnect` stage.
+ *
+ * @param user
+ */
 export async function authenticated(user: AuthenticatedUser | null) {
   getLocalStorage().authenticated(user);
 }
