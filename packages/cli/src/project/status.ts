8074142a3e18baa79b78755593a703702c7215d0
Assaf Arkin <assaf@labnotes.org>
2022-02-03 12:01:21 -0800

    Changed: status now runs in order (lower complexity, quicker response)

--- packages/cli/src/project/status.ts
+++ packages/cli/src/project/status.ts
@@ -16,103 +16,34 @@ import { loadCredentials } from "./project.js";
 const command = new Command("status")
   .description("status of your project")
   .action(async () => {
-    const {
-      current,
-      httpUrl,
-      memory,
-      name,
-      queues,
-      provisioned,
-      region,
-      reserved,
-      schedules,
-      timeout,
-      wsUrl,
-    } = await loadStatus();
+    const { name, awsRegion: region } = await loadCredentials();
+    const lambda = new Lambda({ region });
 
     process.stdout.write(` Name\t\t: ${name}\n`);
 
     if (process.stdout.isTTY) {
       process.stdout.write(
-        chalk.dim(
-          "─".repeat(
-            Math.min(wsUrl.length + 18, process.stdout.getWindowSize()[0])
-          )
-        ) + "\n"
+        chalk.dim("─".repeat(process.stdout.getWindowSize()[0])) + "\n"
       );
     }
 
-    process.stdout.write(` Version\t: ${current?.version ?? "NONE"}\n`);
-    if (!current) return;
-
-    process.stdout.write(
-      ` Code size\t: ${filesize(current.size)} (compressed)\n`
-    );
-    process.stdout.write(` Deployed\t: ${current.modified.toLocaleString()}\n`);
-
-    process.stdout.write(` Region\t\t: ${region}\n`);
-    process.stdout.write(
-      ` Avail memory\t: ${filesize(memory * 10000 * 1000)}\n`
-    );
-    process.stdout.write(` Timeout\t: ${ms(timeout * 1000)}\n`);
-
-    process.stdout.write(
-      ` Reserved\t: ${
-        reserved === 0
-          ? "0 (no instances)"
-          : typeof reserved === "number"
-          ? reserved.toLocaleString()
-          : "no reserve"
-      }\n`
-    );
-    if (provisioned?.length) {
-      provisioned.forEach(({ status, requested, allocated, available }) => {
-        process.stdout.write(` Provisioned\t: ${status}\n`);
-        process.stdout.write(` — Requested\t: ${requested}\n`);
-        process.stdout.write(` — Allocated\t: ${allocated}\n`);
-        process.stdout.write(` — Available\t: ${available}\n`);
-      });
-    } else process.stdout.write(" Provisioned\t: no\n");
-    process.stdout.write("\n");
-
-    process.stdout.write(` HTTP\t\t: ${httpUrl}\n`);
-    process.stdout.write(` WebSocket\t: ${wsUrl}\n`);
-
-    if (queues.length) {
-      process.stdout.write("\n\n");
-      displayTable(
-        ["Queue", "Processed (5m)", "In flight", "Oldest"],
-        queues.map(({ queueName, processed, inFlight, oldest }) => [
-          queueName,
-          processed?.toLocaleString() ?? "0",
-          inFlight?.toLocaleString() ?? "0",
-          oldest ? ms(+oldest * 1000) : "n/a",
-        ])
-      );
-    }
-
-    if (schedules.length) {
-      process.stdout.write("\n\n");
-      displayTable(
-        ["Schedule", "Recurring", "Next run", "Invoked (24h)"],
-        schedules.map(({ name, cron, next, invocations }) => [
-          name,
-          cron,
-          next?.toLocaleString() ?? "never",
-          invocations.toLocaleString(),
-        ])
-      );
-    }
+    const lambdaArn = await showDeployment({ project: name, region });
+    await showConcurrency({ lambda, lambdaArn });
+    await showEndpoints({ project: name, region });
+    await showQueues({ region, project: name });
+    await showSchedules(lambdaArn);
   });
 
-async function loadStatus() {
-  const { name, awsRegion: region } = await loadCredentials();
-  const spinner = ora("Inspecting project").start();
+async function showDeployment({
+  project,
+  region,
+}: {
+  project: string;
+  region: string;
+}) {
+  const spinner = ora("Inspecting deployment").start();
 
-  const [versions, { httpUrl, wsUrl }] = await Promise.all([
-    getRecentVersions({ region, slug: name }),
-    getAPIGatewayURLs({ project: name, region }),
-  ]);
+  const versions = await getRecentVersions({ region, slug: project });
 
   const current = versions.find(({ isCurrent }) => isCurrent);
   if (!current)
@@ -120,50 +51,138 @@ async function loadStatus() {
 
   const lambda = new Lambda({ region });
   const currentArn = current.arn.replace(/:\d+$/, ":current");
+  const { MemorySize: memory, Timeout: timeout } =
+    await lambda.getFunctionConfiguration({
+      FunctionName: currentArn,
+    });
+
+  spinner.stop();
+
+  process.stdout.write(` Version\t: ${current?.version ?? "n/a"}\n`);
+  process.stdout.write(
+    ` Code size\t: ${filesize(current.size)} (compressed)\n`
+  );
+  process.stdout.write(` Deployed\t: ${current.modified.toLocaleString()}\n`);
+  process.stdout.write(` Region\t\t: ${region}\n`);
+  process.stdout.write(
+    ` Avail memory\t: ${filesize(memory! * 10000 * 1000)}\n`
+  );
+  process.stdout.write(` Timeout\t: ${ms(timeout! * 1000)}\n`);
+
+  return currentArn;
+}
+
+async function showEndpoints({
+  project,
+  region,
+}: {
+  project: string;
+  region: string;
+}) {
+  const spinner = ora("Inspecting endpoints").start();
+  const { httpUrl, wsUrl } = await getAPIGatewayURLs({ project, region });
+  spinner.stop();
+
+  process.stdout.write("\n");
+  process.stdout.write(` HTTP\t\t: ${httpUrl}\n`);
+  process.stdout.write(` WebSocket\t: ${wsUrl}\n`);
+}
+
+async function showConcurrency({
+  lambda,
+  lambdaArn,
+}: {
+  lambda: Lambda;
+  lambdaArn: string;
+}) {
+  const spinner = ora("Inspecting concurrency").start();
   // Version/alias not supported when reading concurrency configuration
-  const concurrencyArn = current.arn.replace(/:\d+$/, "");
+  const concurrencyArn = lambdaArn.replace(/:\w+$/, "");
   const [
-    { MemorySize: memory, Timeout: timeout },
     { ReservedConcurrentExecutions: reserved },
     { ProvisionedConcurrencyConfigs: provisioned },
-    queues,
-    schedules,
   ] = await Promise.all([
-    lambda.getFunctionConfiguration({
-      FunctionName: currentArn,
-    }),
     lambda.getFunctionConcurrency({
       FunctionName: concurrencyArn,
     }),
     lambda.listProvisionedConcurrencyConfigs({
       FunctionName: concurrencyArn,
     }),
-    listQueues({ region, period: ms("5m"), project: name }),
-    listSchedules({ lambdaArn: currentArn, period: ms("1d") }),
   ]);
-
   spinner.stop();
 
-  return {
-    name,
-    region,
-    current,
-    memory: memory ?? 128,
-    queues,
-    reserved,
-    provisioned: provisioned?.map(
+  process.stdout.write(
+    ` Reserved\t: ${
+      reserved === 0
+        ? "0 (no instances)"
+        : typeof reserved === "number"
+        ? reserved.toLocaleString()
+        : "no reserve"
+    }\n`
+  );
+  if (provisioned?.length) {
+    provisioned.forEach(
       ({
-        Status: status,
-        RequestedProvisionedConcurrentExecutions: requested,
-        AllocatedProvisionedConcurrentExecutions: allocated,
-        AvailableProvisionedConcurrentExecutions: available,
-      }) => ({ requested, allocated, available, status })
-    ),
-    schedules,
-    timeout: timeout ?? 300,
-    httpUrl,
-    wsUrl,
-  };
+        Status,
+        RequestedProvisionedConcurrentExecutions,
+        AllocatedProvisionedConcurrentExecutions,
+        AvailableProvisionedConcurrentExecutions,
+      }) => {
+        process.stdout.write(` Provisioned\t: ${Status}\n`);
+        process.stdout.write(
+          ` — Requested\t: ${RequestedProvisionedConcurrentExecutions}\n`
+        );
+        process.stdout.write(
+          ` — Allocated\t: ${AllocatedProvisionedConcurrentExecutions}\n`
+        );
+        process.stdout.write(
+          ` — Available\t: ${AvailableProvisionedConcurrentExecutions}\n`
+        );
+      }
+    );
+  } else process.stdout.write(" Provisioned\t: no\n");
+}
+
+async function showQueues({
+  region,
+  project,
+}: {
+  region: string;
+  project: string;
+}) {
+  const spinner = ora("Inspecting queues").start();
+  const queues = await listQueues({ region, period: ms("5m"), project });
+  spinner.stop();
+  if (queues.length) {
+    process.stdout.write("\n\n");
+    displayTable(
+      ["Queue", "Processed (5m)", "In flight", "Oldest"],
+      queues.map(({ queueName, processed, inFlight, oldest }) => [
+        queueName,
+        processed?.toLocaleString() ?? "0",
+        inFlight?.toLocaleString() ?? "0",
+        oldest ? ms(+oldest * 1000) : "n/a",
+      ])
+    );
+  }
+}
+
+async function showSchedules(lambdaArn: string) {
+  const spinner = ora("Inspecting schedules").start();
+  const schedules = await listSchedules({ lambdaArn, period: ms("1d") });
+  spinner.stop();
+  if (schedules.length) {
+    process.stdout.write("\n\n");
+    displayTable(
+      ["Schedule", "Recurring", "Next run", "Invocations (24h)"],
+      schedules.map(({ cron, name, next, invocations }) => [
+        name,
+        cron,
+        next?.toLocaleString() ?? "n/a",
+        invocations?.toLocaleString() ?? "0",
+      ])
+    );
+  }
 }
 
 export default command;
