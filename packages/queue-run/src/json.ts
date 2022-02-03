d7614aff951c26fd6c512c33491d5ba4a9f57c4c
Assaf Arkin <assaf@labnotes.org>
2022-02-03 12:38:26 -0800

    Added: JSONify to cast object into JSON

--- packages/queue-run/src/json.ts
+++ packages/queue-run/src/json.ts
@@ -1,9 +1,49 @@
+/**
+ * This type can represent any JSON value, including primitives, arrays, and objects.
+ */
 export type JSONValue =
   | string
   | number
   | boolean
   | null
+  | undefined
   | JSONObject
-  | JSONValue[]
-  | undefined;
+  | JSONArray;
+
 export type JSONObject = { [key: string]: JSONValue };
+export type JSONArray = JSONValue[];
+
+interface WithToJSON {
+  toJSON(): JSONValue;
+}
+
+/**
+ * Use this to cast any type into a JSONValue.
+ *
+ * ```
+ * type MyObject = {
+ *   name: string;
+ *   date: Date;
+ *   error: Error;
+ *   fn: () => void;
+ * };
+ *
+ * type MyObjectSerialized = JSONify<MyObject>;
+ * =>
+ * {
+ *   name: string;
+ *   date: string; // toJSON
+ *   error: {};
+ *   fn: never;
+ * }
+ * ```
+ */
+export type JSONify<T> = T extends WithToJSON
+  ? ReturnType<T["toJSON"]>
+  : T extends Error
+  ? {}
+  : T extends Function
+  ? never
+  : T extends object
+  ? { [K in keyof T]: JSONify<T[K]> }
+  : T;
