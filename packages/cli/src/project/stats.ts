1c15a3edd7cdee02fcc9e62e87e746aee96d2c2a
Assaf Arkin <assaf@labnotes.org>
2022-02-03 09:32:39 -0800

    Added statistics for Lambda invocations

--- packages/cli/src/project/stats.ts
+++ packages/cli/src/project/stats.ts
@@ -1,117 +1,241 @@
-import { CloudWatch } from "@aws-sdk/client-cloudwatch";
-import { Command } from "commander";
+import { CloudWatch, Datapoint } from "@aws-sdk/client-cloudwatch";
+import { Command, Option } from "commander";
+import ms from "ms";
 import ora from "ora";
-import { displayTable } from "queue-run-builder";
+import { displayTable, getAPIGatewayURLs } from "queue-run-builder";
 import { loadCredentials } from "./project.js";
 
 const command = new Command("stats");
 
+const rangePeriod = new Option(
+  "-r, --range <range>",
+  'time range, eg "3h", "7d"'
+).default("12h");
+
 command
-  .command("invocations")
-  .description("invocations over last 24 hours")
-  .action(async () => {
+  .command("lambda")
+  .description("Lambda invocations")
+  .addOption(rangePeriod)
+  .action(async ({ range }: { range: string }) => {
     const { name, awsRegion: region } = await loadCredentials();
 
-    const spinner = ora("Getting invocation metrics").start();
-    const cloudWatch = new CloudWatch({ region });
-    const period = 60 * 60; // 1 hour
-    const range = period * 24; // 1 day
-    const now = Date.now();
-    const end = now - (now % 60000);
-    const start = end - range * 1000;
     const lambdaName = `qr-${name}`;
 
-    const [invocations, errors, duration] = await Promise.all([
-      getLambdaMetric({
-        cloudWatch,
-        lambdaName,
-        period,
-        start,
-        end,
-        metric: "Invocations",
-      }),
-      getLambdaMetric({
-        cloudWatch,
-        lambdaName,
-        period,
-        start,
-        end,
-        metric: "Errors",
-      }),
-      getLambdaMetric({
-        cloudWatch,
-        lambdaName,
-        period,
-        start,
-        end,
-        metric: "Duration",
-      }),
-    ]);
-
-    const metrics = [];
-    for (let timestamp = start; timestamp < end; timestamp += period * 1000) {
-      metrics.push({
-        timestamp,
-        invocations:
-          invocations.find(
-            ({ Timestamp }) => Timestamp!.getTime() === timestamp
-          )?.Sum ?? 0,
-        errors:
-          errors.find(({ Timestamp }) => Timestamp!.getTime() === timestamp)
-            ?.Sum ?? 0,
-        average: duration.find(
-          ({ Timestamp }) => Timestamp!.getTime() === timestamp
-        )?.Average,
-        maximum: duration.find(
-          ({ Timestamp }) => Timestamp!.getTime() === timestamp
-        )?.Maximum,
-      });
-    }
-    spinner.stop();
+    const metrics = await collectMetrics({
+      dimension: { name: "FunctionName", value: lambdaName },
+      metrics: [
+        { name: "Invocations", aggregate: "Sum" },
+        { name: "Throttles", aggregate: "Sum" },
+        { name: "Errors", aggregate: "Sum" },
+        { name: "ConcurrentExecutions", aggregate: "Maximum" },
+        { name: "Duration", aggregate: "Average" },
+        { name: "Duration", aggregate: "Maximum" },
+      ],
+      namespace: "AWS/Lambda",
+      range: ms(range),
+      region,
+    });
 
     displayTable(
       [
         "Timestamp",
         "Invocations",
+        "Throttled",
         "Errors",
+        "Concurrent",
         "Duration (avg)",
         "Duration (max)",
       ],
-      metrics.map(({ timestamp, invocations, errors, average, maximum }) => [
-        new Date(timestamp).toLocaleTimeString(),
-        invocations.toLocaleString(),
-        errors.toLocaleString(),
-        average ? `${average.toFixed(0)} ms` : "",
-        maximum ? `${maximum.toFixed(0)} ms` : "",
+      metrics.map(
+        ([timestamp, invocations, throttles, errors, concurrent, avg, max]) => [
+          new Date(timestamp!).toLocaleString(),
+          invocations?.toLocaleString() ?? "",
+          throttles?.toLocaleString() ?? "",
+          errors?.toLocaleString() ?? "",
+          concurrent?.toLocaleString() ?? "",
+          avg ? `${avg.toFixed(2)} ms` : "",
+          max ? `${max.toFixed(2)} ms` : "",
+        ]
+      )
+    );
+  });
+
+command
+  .command("http")
+  .description("HTTP requests")
+  .addOption(rangePeriod)
+  .action(async ({ range }: { range: string }) => {
+    const { name, awsRegion: region } = await loadCredentials();
+
+    const { httpApiId } = await getAPIGatewayURLs({ project: name, region });
+
+    const metrics = await collectMetrics({
+      dimension: { name: "ApiId", value: httpApiId },
+      metrics: [
+        { name: "Count", aggregate: "SampleCount" },
+        { name: "4XXError", aggregate: "Sum" },
+        { name: "5XXError", aggregate: "Sum" },
+        { name: "Latency", aggregate: "Average" },
+        { name: "Latency", aggregate: "Maximum" },
+      ],
+      namespace: "AWS/ApiGateway",
+      range: ms(range),
+      region,
+    });
+
+    displayTable(
+      [
+        "Timestamp",
+        "Requests",
+        "4xx",
+        "5xx",
+        "Response Time (avg)",
+        "Response Time (max)",
+      ],
+      metrics.map(([timestamp, requests, code4xx, code5xx, avg, max]) => [
+        new Date(timestamp!).toLocaleString(),
+        requests?.toLocaleString() ?? "",
+        code4xx?.toLocaleString() ?? "",
+        code5xx?.toLocaleString() ?? "",
+        avg ? `${avg.toFixed(2)} ms` : "",
+        max ? `${max.toFixed(2)} ms` : "",
       ])
     );
   });
 
-async function getLambdaMetric({
-  cloudWatch,
-  lambdaName,
-  metric,
-  period,
-  start,
-  end,
-}: {
-  cloudWatch: CloudWatch;
-  lambdaName: string;
-  metric: string;
-  period: number;
-  start: number;
-  end: number;
-}) {
-  const { Datapoints } = await cloudWatch.getMetricStatistics({
-    EndTime: new Date(end),
-    Namespace: "AWS/Lambda",
-    MetricName: metric,
-    Period: period,
-    StartTime: new Date(start),
-    Statistics: ["Sum", "Average", "Maximum"],
-    Dimensions: [{ Name: "FunctionName", Value: lambdaName }],
+command
+  .command("ws")
+  .description("WebSocket connections")
+  .addOption(rangePeriod)
+  .action(async ({ range }: { range: string }) => {
+    const { name, awsRegion: region } = await loadCredentials();
+
+    const { wsApiId } = await getAPIGatewayURLs({ project: name, region });
+
+    const metrics = await collectMetrics({
+      dimension: { name: "ApiId", value: wsApiId },
+      metrics: [
+        { name: "ConnectCount", aggregate: "Sum" },
+        { name: "MessageCount", aggregate: "Sum" },
+        { name: "ExecutionError", aggregate: "Sum" },
+        { name: "IntegrationLatency", aggregate: "Average" },
+        { name: "IntegrationLatency", aggregate: "Maximum" },
+      ],
+      namespace: "AWS/ApiGateway",
+      range: ms(range),
+      region,
+    });
+
+    displayTable(
+      [
+        "Timestamp",
+        "Connections",
+        "Messages",
+        "Errors",
+        "Response Time (avg)",
+        "Response Time (max)",
+      ],
+      metrics.map(([timestamp, connections, messages, errors, avg, max]) => [
+        new Date(timestamp!).toLocaleString(),
+        connections?.toLocaleString() ?? "",
+        messages?.toLocaleString() ?? "",
+        errors?.toLocaleString() ?? "",
+        avg ? `${avg.toFixed(2)} ms` : "",
+        max ? `${max.toFixed(2)} ms` : "",
+      ])
+    );
   });
-  return Datapoints ?? [];
+
+command
+  .command("queue")
+  .description("queued jobs")
+  .argument("<name>", "queue name")
+  .addOption(rangePeriod)
+  .action(async (name, { range }: { range: string }) => {
+    const { name: project, awsRegion: region } = await loadCredentials();
+
+    const queueName = `qr-${project}__${name}`;
+
+    const metrics = await collectMetrics({
+      dimension: { name: "QueueName", value: queueName },
+      metrics: [
+        { name: "NumberOfMessagesSent", aggregate: "Sum" },
+        { name: "NumberOfMessagesDeleted", aggregate: "Sum" },
+        { name: "ApproximateNumberOfMessagesNotVisible", aggregate: "Maximum" },
+        { name: "ApproximateAgeOfOldestMessage", aggregate: "Maximum" },
+      ],
+      namespace: "AWS/SQS",
+      range: ms(range),
+      region,
+    });
+
+    displayTable(
+      ["Timestamp", "Queued", "Processed", "In-flight", "Oldest message"],
+      metrics.map(([timestamp, sent, deleted, inFlight, oldest]) => [
+        new Date(timestamp!).toLocaleString(),
+        sent ? sent.toLocaleString() : "",
+        deleted ? deleted.toLocaleString() : "",
+        inFlight ? inFlight.toLocaleString() : "",
+        oldest ? ms(oldest * 1000) : "",
+      ])
+    );
+  });
+
+async function collectMetrics({
+  dimension,
+  metrics,
+  namespace,
+  range,
+  region,
+}: {
+  dimension: { name: string; value: string };
+  metrics: { name: string; aggregate: keyof Datapoint }[];
+  namespace: string;
+  range: number;
+  region: string;
+}): Promise<Array<Array<number | undefined>>> {
+  const spinner = ora("Collecting queue metrics").start();
+  const cloudWatch = new CloudWatch({ region });
+  const period =
+    range <= ms("1h") ? ms("1m") : range <= ms("7d") ? ms("1h") : ms("1d");
+  const now = Date.now();
+  const end = new Date(now - (now % 60000));
+  const start = new Date(end.getTime() - range);
+
+  const datapoints = (await Promise.all(
+    metrics.map(async (metric) => {
+      const { Datapoints } = await cloudWatch.getMetricStatistics({
+        Dimensions: [{ Name: dimension.name, Value: dimension.value }],
+        EndTime: end,
+        MetricName: metric.name,
+        Namespace: namespace,
+        Period: period / 1000,
+        StartTime: start,
+        Statistics: [metric.aggregate],
+      });
+      return (Datapoints ?? []).map((datapoint) => [
+        datapoint.Timestamp!.getTime(),
+        datapoint[metric.aggregate],
+      ]);
+    })
+  )) as Array<Array<[number, number | undefined]>>;
+
+  const collected = [];
+  for (
+    let timestamp = start.getTime();
+    timestamp < end.getTime();
+    timestamp += period
+  ) {
+    collected.push([
+      timestamp,
+      ...datapoints.map(
+        (datapoints) =>
+          datapoints.find((datapoint) => datapoint[0] === timestamp)?.[1]
+      ),
+    ]);
+  }
+  spinner.stop();
+  return collected;
 }
 
 export default command;
