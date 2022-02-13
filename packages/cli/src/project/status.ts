5bda55f6575fe51fa7fb2c1d238acd52092f87f0
Assaf Arkin <assaf@labnotes.org>
2022-02-13 13:05:49 -0800

    Changed: status command shows useful error message if project not deployed

--- packages/cli/src/project/status.ts
+++ packages/cli/src/project/status.ts
@@ -31,74 +31,87 @@ async function showDeployment({
   region: string;
 }) {
   const spinner = ora("Inspecting deployment").start();
+  try {
+    let versions;
+    try {
+      versions = await getRecentVersions({ region, slug: project });
+    } catch (error) {
+      const { name } = error as { name?: string };
+      if (name === "ResourceNotFoundException") {
+        throw new Error(
+          "No deployment found. Did you deploy your project using `npx queue-run deploy`?"
+        );
+      } else throw error;
+    }
 
-  const [versions, { httpUrl, wsUrl }] = await Promise.all([
-    getRecentVersions({ region, slug: project }),
-    getAPIGatewayURLs({ project, region }),
-  ]);
+    const current = versions.find(({ isCurrent }) => isCurrent);
+    if (!current)
+      throw new Error("No current version found: did you deploy this project?");
 
-  const current = versions.find(({ isCurrent }) => isCurrent);
-  if (!current)
-    throw new Error("No current version found: did you deploy this project?");
+    const { httpUrl, wsUrl } = await getAPIGatewayURLs({ project, region });
 
-  const lambda = new Lambda({ region });
-  const currentArn = current.arn.replace(/:\d+$/, ":current");
-  // Version/alias not supported when reading concurrency configuration
-  const concurrencyArn = current.arn.replace(/:\w+$/, "");
-  const [
-    { MemorySize: memory, Timeout: timeout },
-    { ReservedConcurrentExecutions: reserved },
-    { ProvisionedConcurrencyConfigs: provisioned },
-  ] = await Promise.all([
-    lambda.getFunctionConfiguration({
-      FunctionName: currentArn,
-    }),
-    lambda.getFunctionConcurrency({
-      FunctionName: concurrencyArn,
-    }),
-    lambda.listProvisionedConcurrencyConfigs({
-      FunctionName: concurrencyArn,
-    }),
-  ]);
+    const lambda = new Lambda({ region });
+    const currentArn = current.arn.replace(/:\d+$/, ":current");
+    // Version/alias not supported when reading concurrency configuration
+    const concurrencyArn = current.arn.replace(/:\w+$/, "");
+    const [
+      { MemorySize: memory, Timeout: timeout },
+      { ReservedConcurrentExecutions: reserved },
+      { ProvisionedConcurrencyConfigs: provisioned },
+    ] = await Promise.all([
+      lambda.getFunctionConfiguration({
+        FunctionName: currentArn,
+      }),
+      lambda.getFunctionConcurrency({
+        FunctionName: concurrencyArn,
+      }),
+      lambda.listProvisionedConcurrencyConfigs({
+        FunctionName: concurrencyArn,
+      }),
+    ]);
 
-  spinner.stop();
+    spinner.stop();
 
-  displayTable({
-    headers: ["Project", project],
-    rows: [
-      ["Version", current.version],
-      ["Code size", filesize(current.size)],
-      ["Deployed", localTimestamp(current.modified)],
-      ["Region", region],
-      ["Available memory", filesize(memory! * 10000 * 1000)],
-      ["Timeout", ms(timeout! * 1000)],
-      ["Reserved concurrency", reserved ?? "no reserve"],
-      ...(provisioned
-        ? (provisioned
-            .map((concurrency) => [
-              ["Provisioned concurrency", concurrency.Status],
-              [
-                " - Requested",
-                concurrency.RequestedProvisionedConcurrentExecutions,
-              ],
-              [
-                " - Allocated",
-                concurrency.AllocatedProvisionedConcurrentExecutions,
-              ],
-              [
-                " - Available",
-                concurrency.AvailableProvisionedConcurrentExecutions,
-              ],
-            ])
-            .flat() as [string, string][])
-        : [["Provisioned", "no"]]),
-      ["HTTP", httpUrl],
-      ["WebSocket", wsUrl],
-    ],
-    options: { fullWidth: true, colWidths: [22] },
-  });
+    displayTable({
+      headers: ["Project", project],
+      rows: [
+        ["Version", current.version],
+        ["Code size", filesize(current.size)],
+        ["Deployed", localTimestamp(current.modified)],
+        ["Region", region],
+        ["Available memory", filesize(memory! * 10000 * 1000)],
+        ["Timeout", ms(timeout! * 1000)],
+        ["Reserved concurrency", reserved ?? "no reserve"],
+        ...(provisioned
+          ? (provisioned
+              .map((concurrency) => [
+                ["Provisioned concurrency", concurrency.Status],
+                [
+                  " - Requested",
+                  concurrency.RequestedProvisionedConcurrentExecutions,
+                ],
+                [
+                  " - Allocated",
+                  concurrency.AllocatedProvisionedConcurrentExecutions,
+                ],
+                [
+                  " - Available",
+                  concurrency.AvailableProvisionedConcurrentExecutions,
+                ],
+              ])
+              .flat() as [string, string][])
+          : [["Provisioned", "no"]]),
+        ["HTTP", httpUrl],
+        ["WebSocket", wsUrl],
+      ],
+      options: { fullWidth: true, colWidths: [22] },
+    });
 
-  return currentArn;
+    return currentArn;
+  } catch (error) {
+    spinner.fail();
+    throw error;
+  }
 }
 
 async function showQueues({
