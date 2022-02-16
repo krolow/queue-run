1177fb641c7e3259b901b6bc8dcc46f80e2e02e4
Assaf Arkin <assaf@labnotes.org>
2022-02-16 11:00:13 -0800

    The queue and schedule commands now always run in production

--- packages/cli/src/commands/queue.ts
+++ packages/cli/src/commands/queue.ts
@@ -1,26 +1,17 @@
 import { SQS } from "@aws-sdk/client-sqs";
-import { Command, Option } from "commander";
-import fs from "node:fs/promises";
-import readline from "node:readline";
-import { URL } from "node:url";
+import { Command } from "commander";
 import ora from "ora";
-import invariant from "tiny-invariant";
 import { loadCredentials } from "../shared/config.js";
+import readPayload from "../shared/read_payload.js";
 
 const command = new Command("queue")
-  .description("runs the job using queue handler")
+  .description("manually queue job in production")
   .argument("<queue>", "the queue name")
   .argument(
     "[payload]",
     'JSON or plain text (use @name to read from a file, "-" to read from stdin)'
   )
-  .addOption(
-    new Option("-p, --port <port>", "port to run the server on")
-      .env("PORT")
-      .default(8000)
-  )
   .option("-g --group <group>", "group ID (FIFO queues only)")
-  .option("--prod", "run the job in production", false)
   .addHelpText(
     "after",
     `\n
@@ -40,100 +31,38 @@ $ cat payload.json | npx queue-run dev queue my-queue.fifo -g groupA
       input: string | undefined,
       {
         group,
-        port,
-        prod,
       }: {
         group: string | undefined;
-        port: number;
-        prod: boolean | undefined;
       }
     ) => {
-      if (prod) await queueInProduction({ group, input, queue });
-      else await queueInDevelopment({ group, input, port, queue });
+      const { name, awsRegion: region } = await loadCredentials();
+
+      const payload = await readPayload(input);
+      if (!payload) throw new Error("Cannot queue empty message");
+
+      const spinner = ora("Queuing job").start();
+      const sqs = new SQS({ region });
+
+      try {
+        const { QueueUrl } = await sqs.getQueueUrl({
+          QueueName: `qr-${name}__${queue}`,
+        });
+        await sqs.sendMessage({
+          QueueUrl,
+          MessageBody: payload || "",
+          ...(group ? { MessageGroupId: group } : {}),
+        });
+        spinner.succeed();
+      } catch (error) {
+        spinner.fail();
+        if (
+          (error as { name?: string }).name ===
+          "AWS.SimpleQueueService.NonExistentQueue"
+        )
+          throw new Error(`Queue "${queue}" not found`);
+        else throw error;
+      }
     }
   );
-
-async function queueInProduction({
-  group,
-  input,
-  queue,
-}: {
-  input: string | undefined;
-  group: string | undefined;
-  queue: string;
-}) {
-  const { name, awsRegion: region } = await loadCredentials();
-
-  const payload = await readPayload(input);
-  if (!payload) throw new Error("Cannot queue empty message");
-
-  const spinner = ora("Queuing job").start();
-  const sqs = new SQS({ region });
-
-  try {
-    const { QueueUrl } = await sqs.getQueueUrl({
-      QueueName: `qr-${name}__${queue}`,
-    });
-    invariant(QueueUrl, `Queue ${queue} not found`);
-
-    await sqs.sendMessage({
-      QueueUrl,
-      MessageBody: payload || "",
-      ...(group ? { MessageGroupId: group } : {}),
-    });
-  } catch (error) {
-    if (
-      (error as { name?: string }).name ===
-      "AWS.SimpleQueueService.NonExistentQueue"
-    )
-      throw new Error(`Queue ${queue} not found`);
-    else throw error;
-  } finally {
-    spinner.stop();
-  }
-}
-
-async function queueInDevelopment({
-  group,
-  input,
-  port,
-  queue,
-}: {
-  input: string | undefined;
-  group: string | undefined;
-  port: number;
-  queue: string;
-}) {
-  const payload = await readPayload(input);
-  if (!payload) throw new Error("Cannot queue empty message");
-
-  const path = group ? `/$queues/${queue}/${group}` : `/$queues/${queue}`;
-  await fetch(new URL(path, `http://localhost:${port}`).href, {
-    method: "POST",
-    body: payload,
-  });
-}
-
-async function readPayload(payload: string | undefined): Promise<string> {
-  if (payload === "-") return await fs.readFile("/dev/stdin", "utf-8");
-  else if (payload?.startsWith("@"))
-    return await fs.readFile(payload.slice(1), "utf-8");
-  else if (payload) return payload;
-  else {
-    if (process.stdin.isTTY) {
-      console.info(
-        "Type your message then Ctrl+D on an empty line (Ctrl+C to exit)"
-      );
-    }
-    const rl = readline.createInterface({
-      input: process.stdin,
-      prompt: "",
-    });
-    rl.on("SIGINT", () => process.exit(-1));
-    const lines = [];
-    for await (const line of rl) lines.push(line);
-    return lines.join("\n");
-  }
-}
 
 export default command;
--- packages/cli/src/commands/schedule.ts
+++ packages/cli/src/commands/schedule.ts
@@ -1,55 +1,29 @@
 import { Lambda } from "@aws-sdk/client-lambda";
-import { Command, Option } from "commander";
+import { Command } from "commander";
 import ora from "ora";
 import { loadCredentials } from "../shared/config.js";
 
 const command = new Command("schedule")
-  .description("manually run a scheduled job")
+  .description("manually run a scheduled job in production")
   .argument("<name>", "name of the scheduled job")
-  .option("--prod", "run the job in production", false)
-  .addOption(
-    new Option("-p, --port <port>", "port to run the server on")
-      .env("PORT")
-      .default(8000)
-  )
-  .action(
-    async (
-      schedule,
-      {
-        prod,
-        port,
-      }: {
-        prod: boolean | undefined;
-        port: number;
-      }
-    ) => {
-      if (prod) await scheduleInProduction(schedule);
-      else await scheduleInDevelopment(schedule, port);
+  .action(async (schedule) => {
+    const { name: project, awsRegion: region } = await loadCredentials();
+    const spinner = ora("Running scheduled job").start();
+    try {
+      const lambda = new Lambda({ region });
+      const lambdaName = `qr-${project}`;
+      const payload = { source: "cli.schedule", schedule };
+      const { StatusCode } = await lambda.invoke({
+        FunctionName: lambdaName,
+        InvocationType: "RequestResponse",
+        Payload: Buffer.from(JSON.stringify(payload)),
+      });
+      if (StatusCode !== 200) throw new Error(`No scheduled job "${schedule}"`);
+      spinner.succeed();
+    } catch (error) {
+      spinner.fail();
+      throw error;
     }
-  );
-
-async function scheduleInProduction(schedule: string) {
-  const { name, awsRegion: region } = await loadCredentials();
-  const spinner = ora("Triggering scheduled job").start();
-  const lambda = new Lambda({ region });
-  const lambdaName = `qr-${name}`;
-  const payload = {
-    source: "cli.schedule",
-    schedule,
-  };
-  await lambda.invoke({
-    FunctionName: lambdaName,
-    InvocationType: "Event",
-    Payload: Buffer.from(JSON.stringify(payload)),
   });
-  spinner.stop();
-}
-
-async function scheduleInDevelopment(schedule: string, port: number) {
-  await fetch(
-    new URL(`/$schedules/${schedule}`, `http://localhost:${port}`).href,
-    { method: "POST" }
-  );
-}
 
 export default command;
