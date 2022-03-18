d09b4d5e3a0764fca8fd8395b6353c2af61e1d77
Assaf Arkin <assaf@labnotes.org>
2022-03-18 16:20:17 -0700

    Fixed dependencies

--- packages/builder/src/deploy/cf_template.ts
+++ packages/builder/src/deploy/cf_template.ts
@@ -37,11 +37,13 @@ export async function cfTemplate({
   ].reduce((all, set) => ({ ...all, ...set }), {});
   // @ts-ignore
   const stack = cloudform.default({
+    AWSTemplateFormatVersion: "2010-09-09",
     Description: `Stack for ${lambdaName}`,
     Parameters: {
       LambdaArn: new StringParameter({
         Description: "Lambda function ARN",
       }),
+      LambdaName: new StringParameter({ Default: lambdaName }),
     },
     Resources: resources,
   });
@@ -52,62 +54,59 @@ function getAPIGatewate(lambdaName: string) {
   return {
     ...getHTTPGateway(lambdaName),
     ...getWebsocketGateway(lambdaName),
-    "lambda.invoke": new Lambda.Permission({
+    gatewayInvoke: new Lambda.Permission({
       Action: "lambda:InvokeFunction",
       FunctionName: lambdaName,
       Principal: "apigateway.amazonaws.com",
-    }).dependsOn("lambda.function"),
+    }),
   };
 }
 
 function getHTTPGateway(lambdaName: string) {
-  const RefApiId = Fn.GetAtt("gateway.http.api", "ApiId");
+  const RefApiId = Fn.Ref("httpApi");
   return {
-    "gateway.http.api": new ApiGatewayV2.Api({
+    httpApi: new ApiGatewayV2.Api({
       Description: `QueueRun API gateway for project ${lambdaName}`,
       Name: `qr-http-${lambdaName}`,
       ProtocolType: "HTTP",
     }).deletionPolicy(DeletionPolicy.Delete),
-    "gateway.http.integration": new ApiGatewayV2.Integration({
+    httpIntegration: new ApiGatewayV2.Integration({
       ApiId: RefApiId,
       IntegrationType: "AWS_PROXY",
       IntegrationUri: RefLambdaArn,
       PayloadFormatVersion: "2.0",
       TimeoutInMillis: 30000,
-    }).dependsOn(["gateway.http.api", "lambda.function"]),
-    "gateway.http.route": new ApiGatewayV2.Route({
+    }).dependsOn("httpApi"),
+    httpRoute: new ApiGatewayV2.Route({
       ApiId: RefApiId,
       RouteKey: "ANY /{proxy+}",
-      Target: Fn.Join("/", [
-        "integrations",
-        Fn.GetAtt("gateway.http.integration", "IntegrationId"),
-      ]),
-    }).dependsOn("gateway.http.integration"),
-    "gateway.http.deployment": new ApiGatewayV2.Deployment({
+      Target: Fn.Join("/", ["integrations", Fn.Ref("httpIntegration")]),
+    }).dependsOn("httpIntegration"),
+    httpDeployment: new ApiGatewayV2.Deployment({
       ApiId: RefApiId,
-    }).dependsOn("gateway.http.integration"),
-    "gateway.http.stage": new ApiGatewayV2.Stage({
+    }).dependsOn("httpIntegration"),
+    httpStage: new ApiGatewayV2.Stage({
       ApiId: RefApiId,
-      DeploymentId: Fn.GetAtt("gateway.http.deployment", "DeploymentId"),
+      DeploymentId: Fn.Ref("httpDeployment"),
       StageName: httpStage,
-    }).dependsOn("gateway.http.deployment"),
+    }).dependsOn("httpDeployment"),
   };
 }
 
 function getWebsocketGateway(lambdaName: string) {
-  const RefApiId = Fn.GetAtt("gateway.websockt.api", "ApiId");
+  const RefApiId = Fn.Ref("websocketApi");
   const RefIntegration = Fn.Join("/", [
     "integrations",
-    Fn.GetAtt("gateway.websocket.integration", "IntegrationId"),
+    Fn.Ref("websocketIntegration"),
   ]);
 
   return {
-    "gateway.websocket": new ApiGatewayV2.Api({
+    websocketApi: new ApiGatewayV2.Api({
       Name: `qr-ws-${lambdaName}`,
       ProtocolType: ProtocolType.WEBSOCKET,
       RouteSelectionExpression: "*",
     }).deletionPolicy(DeletionPolicy.Delete),
-    "gateway.websockt.integration": new ApiGatewayV2.Integration({
+    websocketIntegration: new ApiGatewayV2.Integration({
       ApiId: RefApiId,
       ContentHandlingStrategy: "CONVERT_TO_TEXT",
       IntegrationMethod: "POST",
@@ -115,48 +114,50 @@ function getWebsocketGateway(lambdaName: string) {
       IntegrationUri: Fn.Join("/", [
         "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions",
         RefLambdaArn,
-        "nvocations",
+        "invocations",
       ]),
       PassthroughBehavior: "WHEN_NO_MATCH",
       TimeoutInMillis: 29000,
-    }).dependsOn(["gateway.websocket.api", "lambda.function"]),
-    "gateway.websocket.connect": new ApiGatewayV2.Route({
+    }).dependsOn("websocketApi"),
+    websocketConnect: new ApiGatewayV2.Route({
       ApiId: RefApiId,
       AuthorizationType: "NONE",
       RouteKey: "$connect",
       Target: RefIntegration,
-    }).dependsOn("gateway.wesocket.integration"),
-    "gateway.websocket.disconnect": new ApiGatewayV2.Route({
+    }).dependsOn("websocketIntegration"),
+    websocketDisconnect: new ApiGatewayV2.Route({
       ApiId: RefApiId,
       RouteKey: "$disconnect",
       Target: RefIntegration,
-    }).dependsOn("gateway.wesocket.integration"),
-    "gateway.websocket.default": new ApiGatewayV2.Route({
+    }).dependsOn("websocketIntegration"),
+    websocketDefault: new ApiGatewayV2.Route({
       ApiId: RefApiId,
       RouteKey: "$default",
       Target: RefIntegration,
-    }).dependsOn("gateway.wesocket.integration"),
-    "gateway.websocket.deployment": new ApiGatewayV2.Deployment({
+    }).dependsOn("websocketIntegration"),
+    websocketDeployment: new ApiGatewayV2.Deployment({
       ApiId: RefApiId,
-    }).dependsOn("gateway.wesocket.integration"),
-    "gateway.websocket.stage": new ApiGatewayV2.Stage({
+    }).dependsOn("websocketIntegration"),
+    websocketStage: new ApiGatewayV2.Stage({
       ApiId: RefApiId,
-      DeploymentId: Fn.GetAtt("gateway.websocket.deployment", "DeploymentId"),
+      DeploymentId: Fn.Ref("websocketDeployment"),
       StageName: wsStage,
-    }).dependsOn("gateway.wesocket.deployment"),
+    }).dependsOn("websocketDeployment"),
   };
 }
 
 function getLogs(lambdaName: string) {
   const logGroupName = `/aws/lambda/${lambdaName}`;
   return {
-    "logs.group": new Logs.LogGroup({ LogGroupName: logGroupName }),
+    logsGroup: new Logs.LogGroup({ LogGroupName: logGroupName }).deletionPolicy(
+      DeletionPolicy.Retain
+    ),
   };
 }
 
 function getRole(lambdaName: string) {
   return {
-    "lambda.role": new IAM.Role({
+    lambdaRole: new IAM.Role({
       Path: lambdaRolePath,
       RoleName: lambdaName,
       AssumeRolePolicyDocument: assumeRolePolicy,
@@ -171,9 +172,9 @@ function getQueues(
 ): {
   [key: string]: ResourceBase;
 }[] {
-  return queues.map((queue) => {
+  return queues.map((queue, index) => {
     const { queueName } = queue;
-    const queueId = `sqs.${queueName}.queue`;
+    const queueId = `sqsQueue${index}`;
     return {
       [queueId]: new SQS.Queue({
         QueueName: `${lambdaName}__${queueName}`,
@@ -186,21 +187,22 @@ function getQueues(
             }
           : undefined),
         VisibilityTimeout: queue.timeout * 6,
-      }),
-      [`sqs.${queueName}.source`]: new Lambda.EventSourceMapping({
+      }).deletionPolicy(DeletionPolicy.Delete),
+
+      [`sqsSource${index}`]: new Lambda.EventSourceMapping({
         Enabled: true,
-        EventSourceArn: Fn.GetAtt(queueId, "Arn"),
+        EventSourceArn: Fn.Ref(queueId),
         FunctionName: RefLambdaArn,
         FunctionResponseTypes: ["ReportBatchItemFailures"],
-      }).dependsOn([queueId, "lambda.function"]),
+      }).dependsOn(queueId),
     };
   });
 }
 
 function getSchedules(lambdaName: string, schedules: Manifest["schedules"]) {
-  return schedules.map((schedule) => {
+  return schedules.map((schedule, index) => {
     if (schedule.cron === null) return {};
-    const ruleId = `cloudwatch.schedule.${schedule.name}.rule`;
+    const ruleId = `cloudwatchSchedule${index}`;
     return {
       [ruleId]: new Events.Rule({
         Name: `${lambdaName}.${schedule.name}`,
@@ -209,15 +211,14 @@ function getSchedules(lambdaName: string, schedules: Manifest["schedules"]) {
         )})`,
         State: "ENABLED",
         Targets: [{ Id: "lambda", Arn: RefLambdaArn }],
-      }),
+      }).deletionPolicy(DeletionPolicy.Delete),
 
-      [`cloudwatch.schedule.${schedule.name}.premission`]:
-        new Lambda.Permission({
-          FunctionName: lambdaName,
-          Action: "lambda:InvokeFunction",
-          Principal: "events.amazonaws.com",
-          SourceArn: Fn.GetAtt(ruleId, "Arn"),
-        }).dependsOn([ruleId, "lambda.function"]),
+      [`cloudwatchPermission${index}`]: new Lambda.Permission({
+        FunctionName: lambdaName,
+        Action: "lambda:InvokeFunction",
+        Principal: "events.amazonaws.com",
+        SourceArn: Fn.Ref(ruleId),
+      }),
     };
   });
 }
@@ -237,8 +238,8 @@ const assumeRolePolicy = {
 
 const RefRegion = Fn.Ref("AWS::Region");
 const RefAccountId = Fn.Ref("AWS::AccountId");
-const RefFunctionName = Fn.GetAtt("lambda.function", "FunctionName");
-const RefLogGroup = Fn.Join("", ["/aws/lambda/", RefFunctionName]);
+const RefLambdaName = Fn.Ref("LambdaName");
+const RefLogGroup = Fn.Join("", ["/aws/lambda/", RefLambdaName]);
 
 const lambdaPolicy = {
   Version: "2012-10-17",
@@ -258,7 +259,7 @@ const lambdaPolicy = {
         "arn:aws:sqs",
         RefRegion,
         RefAccountId,
-        Fn.Join("__", [RefFunctionName, "*"]),
+        Fn.Join("__", [RefLambdaName, "*"]),
       ]),
     },
     {
@@ -269,7 +270,7 @@ const lambdaPolicy = {
           "arn:aws:execute-api",
           RefRegion,
           RefAccountId,
-          Fn.Join("", [Fn.GetAtt("api.ws", "ApiId"), "/_ws/*"]),
+          Fn.Join("", [Fn.Ref("websocketApi"), "/_ws/*"]),
         ]),
       ],
     },
